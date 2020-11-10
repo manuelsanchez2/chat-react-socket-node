e2498589c3fcba9a2c5e2485551c5c660a35caeb
manuelsanchez2 <manusansan22@gmail.com>
2020-11-10 15:37:27 +0100

    Make the router work

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
