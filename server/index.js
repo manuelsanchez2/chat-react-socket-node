b9ae44e73801f51365646842b46abe969112eea8
manuelsanchez2 <manusansan22@gmail.com>
2020-11-20 10:32:18 +0100

    Improve some things server-side

--- server/index.js
+++ server/index.js
@@ -13,15 +13,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(router);
-
 app.use(cors());
+app.use(router);
 
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
+
+    socket.join(user.room);
     // This is a message that only you are going to receive as an user
 
     socket.emit("message", {
@@ -40,8 +41,6 @@ io.on("connection", (socket) => {
       users: getUsersInRoom(user.room),
     });
 
-    socket.join(user.room);
-
     callback();
   });
 
@@ -49,22 +48,21 @@ io.on("connection", (socket) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
-    io.to(user.room).emit("message", {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
 
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User abandoned the session");
     const user = removeUser(socket.id);
 
     if (user) {
       io.to(user.room).emit("message", {
         user: "admin",
         text: `${user.name} left the room`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
       });
     }
   });
